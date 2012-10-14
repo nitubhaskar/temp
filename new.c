f359dc42218158a198d510e43faac3c9a5591e92
nitubhaskar <nitubhaskar@gmail.com>
2012-10-14 16:03:10 +0530

    wrote about new.c

---
+++ new.c
@@ -0,0 +1,3 @@
+well not a actual file.. trying to learn about git.. as i screwed with my repo by mistake last time.. and ended up uploading everything again.. so trying to learn now before doing something,,..
+
+hope no one minds.. :)
